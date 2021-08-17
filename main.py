8ae86bf5066f9a9b8080f57c0f957874e826b4ea
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-17 03:15:42 +0000

    updated

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print(what to print')")
 
-# print("Hello World!\nHello World!\nHello World!")
+#String manipulation
+#new line character
+print("Hello World!\nHello World!\nHello World!")
+
+#string concatenation
+print("Hello" + " " +"Sahil")
