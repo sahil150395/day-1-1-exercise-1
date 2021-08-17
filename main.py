e71ad77aaa0e0e0c0486f6406b6421c4aff762f6
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-17 03:08:53 +0000

    test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Write your code below this line 👇
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print('what to print')")
+print("print(what to print')")
+
+# print("Hello World!\nHello World!\nHello World!")
